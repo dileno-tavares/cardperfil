f953bb4e9effcb894c59326102684623ad8ce6a1
Dileno Tavares <74273172+dileno-tavares@users.noreply.github.com>
2024-10-09 21:01:01 -0300

    Card Perfil
    Objetivo do Projeto
    
    A proposta do projeto seria criar um aplicativo mobile com React Native. Para poder colocar em prática os conceitos adquiridos em aula como: criar componente App (tela do aplicativo); fazer o agrupamento de componentes com a View; exibir textos com o Text; exibir imagens com o Image; criar estilo do aplicativo uzando o código StyleSheet. Foi exigido conceitos simples no projeto para dar a oportunidade do aluno criar até mesmo sem ter conhecimento muito técnico na área. Portanto fiz de acordo com o meu conhecimento e expiência que adquiri ao longo dos na área em questão.

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from  'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import Capa from './assets/capa-card.png';
+import Foto from './assets/foto-dev.png';
+
+export default function CardPerfil() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={estilos.container}>
+      <Image
+        source={Capa}
+        style={{ width: 390, height: 480, marginTop: -90, marginBottom: 10 }} /> 
+      <Image
+        source={Foto}
+        style={estilos.img}
+      />
+      <Text style={estilos.titulo}>Dileno Tavares</Text>
+      <Text style={estilos.descricao}>Desenvolvedor Web Full Stack. Tecnólogo em análise e Desenvolvimento de Sistemas. Conhecimento em Php, javascript, Python, Java, Spring Boot, PostgreSQL e MySQL.  
+.</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  img: {
+    width: 180,
+    height: 180,
+    margin: 5,
+    borderRadius: 90,
+  },
+  titulo: {
+    fontSize: 22,
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  descricao: {
+    fontSize: 18,
+    color: '#555',
+    textAlign: 'center',
   },
 });
